5b13f4315bb6d5842d78ce56df1cb515caf4c938
Shanon Levenherz <shanonvl@gmail.com>
2014-08-03 18:36:51 -0400

    js _ templates

---
+++ js/templates/object.js
@@ -0,0 +1,23 @@
+/* jshint */
+;(function() {
+
+    /**
+     * Creates a new instance of this object.
+     */
+    <%= className %> = function() {
+
+    };
+
+    <%= className %>.prototype = {
+
+        /**
+         * This is a sample function
+         * @param args {Object} arguments.
+         */
+        sampleFunction:function(args) {
+
+        }
+
+    };
+
+}).call(this);
